eb5c56d292e61c44007b33052fac034f7323d25f
Maharlika Alberca <maharlikaalberca@gmail.com>
2022-11-06 12:26:41 +0800

    installed tailwind css

--- src/Components/WeatherPage/WeatherPage.tsx
+++ src/Components/WeatherPage/WeatherPage.tsx
@@ -1,9 +1,9 @@
-import React, { useState} from 'react';
+import React from 'react';
 
 function Weather() {
     return (
         <div>
-            <h1>WEATHER PAGE</h1>
+            <h1 className="text-3xl font-bold underline">WEATHER PAGE</h1>
         </div>
     )
 }
